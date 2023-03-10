a341a3871bcdf5dea6da2d97b0cb0d8afc7300ef
YJK0805 <jerryko103@gmail.com>
2023-03-10 09:24:36 +0800

    week4 & birthday

--- 3rd/week4/D1109070_01.cpp
+++ 3rd/week4/D1109070_01.cpp
@@ -3,25 +3,25 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    map<string,int> mp;
-    string s,s1;
+    set<string> st;
+    string s;
     while(getline(cin,s)){
-    	s1 = "";
+    	string s1 = "";
     	for(int i = 0;i < s.size();i++){
     		if(isalpha(s[i])){
     			s1 += tolower(s[i]);
     		}else{
     			if(s1 != ""){
-	    			mp[s1] = 1;
+	    			st.insert(s1);
 	    			s1 = "";
 	    		}
     		}
     	}
     	if(s1 != ""){
-			mp[s1] = 1;
+			st.insert(s1);
 		}
     }
-    for(auto &[i,j] : mp){
+    for(auto &i : st){
     	cout << i << "\n";
     }
     return 0;
